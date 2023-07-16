394c0afe356bd918e0c98b83cbf4691a35008e04
Anders Piniesjö <pugo@pugo.org>
2023-07-16 02:53:32 +0200

    Added doxygen comments

--- z21/z21.h
+++ z21/z21.h
@@ -96,55 +96,249 @@ public:
 
     Z21Status& z21_status() { return m_z21_status; }
 
-    void get_serial_number();                                           // LAN_GET_SERIAL_NUMBER
-    void get_feature_set();                                             // LAN_GET_CODE
-    void get_hardware_info();                                           // LAN_GET_HWINFO
-    void logoff();                                                      // LAN_LOGOFF
 
-    void xbus_get_version();                                            // LAN_X_GET_VERSION
-    void xbus_get_status();                                             // LAN_X_GET_STATUS
-    void xbus_set_track_power_off();                                    // LAN_X_SET_TRACK_POWER_OFF
-    void xbus_set_track_power_on();                                     // LAN_X_SET_TRACK_POWER_ON
+    // =========================================================================================
+    //   Z21 low level API
+    // =========================================================================================
 
-    void xbus_dcc_read_register(uint8_t reg);                           // LAN_X_DCC_READ_REGISTER
-    void xbus_cv_read(uint16_t cv);                                     // LAN_X_CV_READ
-    void xbus_dcc_write_register(uint8_t reg, uint8_t value);           // LAN_X_DCC_WRITE_REGISTER
-    void xbus_cv_write(uint16_t cv, uint8_t value);                     // LAN_X_CV_WRITE
-    void xbus_mm_write_byte(uint8_t reg, uint8_t value);                // LAN_X_MM_WRITE_BYTE
-    void xbus_get_turnout_info(uint16_t address);                       // LAN_X_GET_TURNOUT_INFO
-    void xbus_get_ext_accessory_info(uint16_t address);                 // LAN_X_GET_EXT_ACCESSORY_INFO
-    void xbus_set_turnout(uint16_t address, uint8_t value);             // LAN_X_SET_TURNOUT
-    void xbus_set_ext_accessory(uint16_t address, uint8_t state);       // LAN_X_SET_EXT_ACCESSORY
-    void xbus_set_stop();     // LAN_X_SET_STOP
+    /**
+     * Request Z21 Serial number (LAN_GET_SERIAL_NUMBER).
+     */
+    void get_serial_number();
 
-    void xbus_get_loco_info(uint16_t address);                                                                                  // LAN_X_GET_LOCO_INFO
-    void xbus_set_loco_drive(uint16_t address, uint8_t speed, bool forward);                                                    // LAN_X_SET_LOCO_DRIVE
-    void xbus_set_loco_function(uint16_t address, uint8_t function);                                                            // LAN_X_SET_LOCO_FUNCTION
-    void xbus_set_loco_function_group(uint16_t address, LanX_SetLocoFunctionGroup::FunctionGroup group, uint8_t functions);     // LAN_X_SET_LOCO_FUNCTION_GROUP
-    void xbus_set_loco_binary_state(uint16_t address, bool on, uint8_t binary_address);                                         // LAN_X_SET_LOCO_BINARY_STATE
+    /**
+     * Request Z21 feature set (LAN_GET_CODE).
+     */
+    void get_feature_set();
 
-    void xbus_cv_pom_write_byte(uint16_t address, uint16_t cv, uint8_t value);                                                  // LAN_X_CV_POM_WRITE_BYTE
-    void xbus_cv_pom_write_bit(uint16_t address, uint16_t cv, uint8_t bit_position, uint8_t value);                             // LAN_X_CV_POM_WRITE_BIT
-    void xbus_cv_pom_read_byte(uint16_t address, uint16_t cv);                                                                  // LAN_X_CV_POM_READ_BYTE
-    void xbus_cv_pom_accessory_write_byte(uint16_t address, PomAccessorySelection selction, uint8_t output, uint16_t cv, uint8_t value);                                // LAN_X_CV_POM_ACCESSORY_WRITE_BYTE
-    void xbus_cv_pom_accessory_write_bit(uint16_t address, PomAccessorySelection selction, uint8_t output, uint16_t cv, uint8_t bit_position, uint8_t value);           // LAN_X_CV_POM_ACCESSORY_WRITE_BIT
-    void xbus_cv_pom_accessory_read_byte(uint16_t address, PomAccessorySelection selction, uint8_t output, uint16_t cv);        // LAN_X_CV_POM_ACCESSORY_READ_BYTE
+    /**
+     * Request Z21 hardware info (LAN_GET_HWINFO).
+     */
+    void get_hardware_info();
 
-    void xbus_get_firmware_version();                                   // LAN_X_GET_FIRMWARE_VERSION
+    /**
+     * Logoff from Z21 (LAN_LOGOFF).
+     */
+    void logoff();
 
-    void set_broadcast_flags();                         // LAN_SET_BROADCASTFLAGS
-    void get_broadcast_flags();                         // LAN_GET_BROADCASTFLAGS
+    /**
+     * Request XBus: version (LAN_X_GET_VERSION).
+     */
+    void xbus_get_version();
 
-    void get_loco_mode(uint16_t address);                         // LAN_GET_LOCOMODE
-    void set_loco_mode(uint16_t address, Locomode mode);                         // LAN_SET_LOCOMODE
+    /**
+     * Request XBus: status (LAN_X_GET_STATUS).
+     */
+    void xbus_get_status();
 
-    void get_turnout_mode(uint16_t address);                         // LAN_GET_LOCOMODE
-    void set_turnout_mode(uint16_t address, Locomode mode);                         // LAN_SET_LOCOMODE
+    /**
+     * Request XBus: set track power off (LAN_X_SET_TRACK_POWER_OFF).
+     */
+    void xbus_set_track_power_off();
 
-    void systemstate_get_data();                        // LAN_SYSTEMSTATE_GETDATA
+    /**
+     * Request XBus: set track power on (LAN_X_SET_TRACK_POWER_ON).
+     */
+    void xbus_set_track_power_on();
 
+    /**
+     * Request XBus: read DCC register (LAN_X_DCC_READ_REGISTER).
+     * @param reg register to read
+     */
+    void xbus_dcc_read_register(uint8_t reg);
 
+    /**
+     * Request XBus: read CV value (LAN_X_CV_READ).
+     * @param cv CV to read
+     */
+    void xbus_cv_read(uint16_t cv);
 
+    /**
+     * Request XBus: write DCC register (LAN_X_DCC_WRITE_REGISTER).
+     * @param reg register to write
+     * @param value value to write
+     */
+    void xbus_dcc_write_register(uint8_t reg, uint8_t value);
+
+    /**
+     * Request XBus: write CV (LAN_X_CV_WRITE).
+     * @param cv CV to write
+     * @param value value to write
+     */
+    void xbus_cv_write(uint16_t cv, uint8_t value);
+
+    /**
+     * Request XBus: write MM byte (LAN_X_MM_WRITE_BYTE).
+     * @param reg register to write
+     * @param value value to write
+     */
+    void xbus_mm_write_byte(uint8_t reg, uint8_t value);
+
+    /**
+     * Request XBus: get turnout info (LAN_X_GET_TURNOUT_INFO).
+     * @param address turnout address
+     */
+    void xbus_get_turnout_info(uint16_t address);
+
+    /**
+     * Request XBus: get ext accessory info (LAN_X_GET_EXT_ACCESSORY_INFO).
+     * @param address accessory address
+     */
+    void xbus_get_ext_accessory_info(uint16_t address);
+
+    /**
+     * Request XBus: set turnout value (LAN_X_SET_TURNOUT).
+     * @param address turnout address
+     * @param value value to set
+     */
+    void xbus_set_turnout(uint16_t address, uint8_t value);
+
+    /**
+     * Request XBus: set ext accessory (LAN_X_SET_EXT_ACCESSORY).
+     * @param address accessory address
+     * @param state accessory state
+     */
+    void xbus_set_ext_accessory(uint16_t address, uint8_t state);
+
+    /**
+     * Reqyest XBus: set stop (LAN_X_SET_STOP).
+     */
+    void xbus_set_stop();
+
+    /**
+     * Request XBus: get loco info (LAN_X_GET_LOCO_INFO).
+     * @param address loco address
+     */
+    void xbus_get_loco_info(uint16_t address);
+
+    /**
+     * Request XBus: set loco drive (LAN_X_SET_LOCO_DRIVE).
+     * @param address loco address
+     * @param speed loco speed
+     * @param forward loco direction
+     */
+    void xbus_set_loco_drive(uint16_t address, uint8_t speed, bool forward);
+
+    /**
+     * Request XBus: set loco function (LAN_X_SET_LOCO_FUNCTION).
+     * @param address loco address
+     * @param function loco function
+     */
+    void xbus_set_loco_function(uint16_t address, uint8_t function);
+
+    /**
+     * Request XBus: set loco function group (LAN_X_SET_LOCO_FUNCTION_GROUP).
+     * @param address loco address
+     * @param group loco group
+     * @param functions loco functions
+     */
+    void xbus_set_loco_function_group(uint16_t address, LanX_SetLocoFunctionGroup::FunctionGroup group, uint8_t functions);
+
+    /**
+     * Request XBus: set loco binary state (LAN_X_SET_LOCO_BINARY_STATE).
+     * @param address loco address
+     * @param on `true` if bit should be set to on, otherwise `false`
+     * @param binary_address address to set
+     */
+    void xbus_set_loco_binary_state(uint16_t address, bool on, uint8_t binary_address);
+
+    /**
+     * Request XBus: program on main, write byte (LAN_X_CV_POM_WRITE_BYTE).
+     * @param address loco address to program
+     * @param cv CV to set
+     * @param value value to write
+     */
+    void xbus_cv_pom_write_byte(uint16_t address, uint16_t cv, uint8_t value);
+
+    /**
+     * Request XBus: program on main, write bit (LAN_X_CV_POM_WRITE_BIT).
+     * @param address loco address to program
+     * @param cv CV to set
+     * @param bit_position bit position to write
+     * @param value value to write
+     */
+    void xbus_cv_pom_write_bit(uint16_t address, uint16_t cv, uint8_t bit_position, uint8_t value);
+
+    /**
+     * Request XBus: program on main, read byte (LAN_X_CV_POM_READ_BYTE)
+     * @param address loco address to read
+     * @param cv CV to read
+     */
+    void xbus_cv_pom_read_byte(uint16_t address, uint16_t cv);
+
+    /**
+     * Request XBus: program on main, write accessory byte (LAN_X_CV_POM_ACCESSORY_WRITE_BYTE)
+     * @param address accessory address
+     * @param selction accessory selection
+     * @param output accessory output
+     * @param cv CV to write
+     * @param value value to write
+     */
+    void xbus_cv_pom_accessory_write_byte(uint16_t address, PomAccessorySelection selction, uint8_t output, uint16_t cv, uint8_t value);
+    /**
+     * Request XBus: program on main, write accessory bit (LAN_X_CV_POM_ACCESSORY_WRITE_BIT)
+     * @param address accessory address
+     * @param selction accessory selection
+     * @param output accessory output
+     * @param cv CV to write
+     * @param bit_position bit position to write
+     * @param value value to write
+     */
+    void xbus_cv_pom_accessory_write_bit(uint16_t address, PomAccessorySelection selction, uint8_t output, uint16_t cv, uint8_t bit_position, uint8_t value);
+
+    /**
+     * Request XBus: program on main, read accessory byte (LAN_X_CV_POM_ACCESSORY_READ_BYTE)
+     * @param address accessory address
+     * @param selction accessory selection
+     * @param output accessory output
+     * @param cv
+     */
+    void xbus_cv_pom_accessory_read_byte(uint16_t address, PomAccessorySelection selction, uint8_t output, uint16_t cv);
+
+    /**
+     * Request XBus: get firmware version (LAN_X_GET_FIRMWARE_VERSION).
+     */
+    void xbus_get_firmware_version();
+
+    /**
+     * Set Z21 broadcast flags (LAN_SET_BROADCASTFLAGS).
+     */
+    void set_broadcast_flags();
+
+    /**
+     * Get Z21 broadcast flags (LAN_GET_BROADCASTFLAGS).
+     */
+    void get_broadcast_flags();
+
+    /**
+     * Request lock mode (LAN_GET_LOCOMODE).
+     * @param address loco address
+     */
+    void get_loco_mode(uint16_t address);
+
+    /**
+     * Request to set loco mode (LAN_SET_LOCOMODE).
+     * @param address loco address
+     * @param mode loco mode
+     */
+    void set_loco_mode(uint16_t address, Locomode mode);
+
+    /**
+     * Request to get turnout mode (LAN_GET_TURNOUTMODE).
+     * @param address turnout address
+     */
+    void get_turnout_mode(uint16_t address);
+
+    /** Request to set turnout mode (LAN_SET_TURNOUTMODE).
+     * @param address turnout address
+     * @param mode turnout mode
+     */
+    void set_turnout_mode(uint16_t address, Locomode mode);
+
+    /**
+     * Request system state (LAN_SYSTEMSTATE_GETDATA).
+     */
+    void systemstate_get_data();
 
 private:
     void listen_thread_fn();
